15be414fe008286539d3802e44de9725b17af075
kenseitrg <kensei.trg@gmail.com>
2018-09-03 12:00:27 +0300

    Added edit profile functionality

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-from app import app
+from app import app, db
 from app.models import User, Post
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
